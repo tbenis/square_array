6d988fc4fb333738977333cba7d379be8f7478db
Benis Tambe <will.jenifer@yahoo.com>
2021-01-23 22:56:27 -0500

    commit tree 38e1ffab68ae650a711d9db2387ca7b20e9d0297

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  newArr = []
+  array.each do |number|
+    item  = number ** 2
+    newArr << item
+    
+  end
+  puts "<<<<<<"
+  return newArr 
 end
